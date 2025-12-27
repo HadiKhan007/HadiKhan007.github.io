429702b62fabd4765e82cbf3132f341a871f6950
shahzainaslam11 <shahzainaslam11@outlook.com>
2025-12-27 23:16:15 +0500

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/HadiKhan007.github.io",
+  assetPrefix: "/HadiKhan007.github.io/",
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
