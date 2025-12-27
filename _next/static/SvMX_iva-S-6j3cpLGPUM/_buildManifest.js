646b354ed51fc25633ce13a7750a85efc64063be
HadiKhan007 <alihaidercs17@gmail.com>
2025-12-28 00:56:02 +0500

    Updates

--- _next/static/SvMX_iva-S-6j3cpLGPUM/_buildManifest.js
+++ _next/static/SvMX_iva-S-6j3cpLGPUM/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/HadiKhan007.github.io//_next/:path+",
-        "destination": "/HadiKhan007.github.io/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
